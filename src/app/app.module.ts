b1fa928c126cb893017932eb6f79c118047ff4a1
Wigwa <lucywigwa7@gmail.com>
2020-06-17 12:53:50 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { DatePipePipe } from './date-pipe.pipe';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    QuotesFormComponent,
+    DatePipePipe,
+    HighlightQuoteDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/date-pipe.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { DatePipePipe } from './date-pipe.pipe';
+
+describe('DatePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DatePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
+
